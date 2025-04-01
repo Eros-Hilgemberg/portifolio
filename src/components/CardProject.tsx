8d41e778f224d59e4e88b504c588a08273403e82
EgServices <eroshilgemberg@gmail.com>
2025-03-31 22:56:10 -0300

    image bug fix, add projects

--- src/components/CardProject.tsx
+++ src/components/CardProject.tsx
@@ -10,17 +10,11 @@ function CardProject({ title, description, image }: CardProps) {
     <motion.div
       whileInView={{ scale: [0.8, 1], opacity: [0.1, 1] }}
       transition={{ duration: 0.8 }}
-      className="flex flex-col p-3 gap-2  md:gap-3 border-2 flex-1/2 h-60 md:h-80 rounded-xl md:flex-none md:w-1/4 border-gray-secundary"
+      className="flex flex-col p-3 gap-6 md:gap-3 border-2 flex-1/2 h-60 md:h-80 rounded-xl md:flex-none md:w-1/4 border-gray-secundary"
     >
-      <div
-        className="h-2/3 rounded-xl"
-        style={{
-          background: `url(${image})`,
-          backgroundSize: "contain",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        }}
-      ></div>
+      <div className="h-2/3 rounded-xl">
+        <img src={image} className="w-full h-full" />
+      </div>
       <div className="flex flex-col text-sm md:text-lg rounded-sm 1/3">
         <h3 className="font-bold  uppercase text-sm md:text-lg">{title}</h3>
         <p className="">{description}</p>
--- src/data/Projects.ts
+++ src/data/Projects.ts
@@ -1,7 +1,7 @@
 const Projects = [
   {
-    title: "Culture Register front",
-    description: "Sitema de gerenciamento de cadastros culturais",
+    title: "Culture Register",
+    description: "Gerenciamento de cadastros culturais",
     image: "/imageProjects/CultureRegister.png",
     tag: "front-end,back-end",
   },
@@ -10,6 +10,18 @@ const Projects = [
     description: "Portifólio de projetos pessoais",
     image: "/imageProjects/Portifólio.png",
     tag: "front-end, design",
+  },
+  {
+    title: "Beau",
+    description: "Gerenciador de agendamentos",
+    image: "/imageProjects/Beau.png",
+    tag: "design",
+  },
+  {
+    title: "ApiConect",
+    description: "Front Api de bot whatsapp",
+    image: "/imageProjects/ApiConect.png",
+    tag: "front-end,design",
   },
 ];
 export default Projects;
